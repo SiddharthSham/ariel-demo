95596ea182cde1763ae0363ffb3072f61b09d39f
siddharthsham <siddhsham@gmail.com>
2022-02-14 11:28:01 +0530

    style updates

--- containers/staff/control/index.js
+++ containers/staff/control/index.js
@@ -24,7 +24,61 @@ const Switchable = () => {
   return (
     <>
       <div>{switched ? "On" : "Off"}</div>
-      <Switch checked={switched} onClick={() => toggleSwitch()} />
+      <Switch
+        sx={{
+          backgroundColor: switched ? "#8486ec" : "#F2F2F2",
+          borderRadius: "12.5px",
+          width: "60px",
+          height: "25px",
+          border: "none",
+          transition: "background-color 0.2s ease-out"
+        }}
+        className={S.switch}
+        checked={switched}
+        onClick={() => toggleSwitch()}
+      />
+    </>
+  );
+};
+
+const Counter = ({
+  defaultCount = 0,
+  min = Number.NEGATIVE_INFINITY,
+  max = Number.POSITIVE_INFINITY,
+  step = 1,
+}) => {
+  const [count, setCount] = useState(defaultCount);
+
+  const increment = () => {
+    if (count + step > max) return;
+    setCount(count + step);
+  };
+  const decrement = () => {
+    if (count - step < min) return;
+    setCount(count - step);
+  };
+
+  return (
+    <>
+      <div className={S.counter}>
+        <button onClick={decrement} className={S.counterDecrement}>
+          -
+        </button>
+        <input
+          type={"number"}
+          value={count}
+          min={min}
+          max={max}
+          onChange={(e) =>
+            setCount(
+              parseInt(e.target.value) ? parseInt(e.target.value) : defaultCount
+            )
+          }
+        />
+        <button onClick={increment} className={S.counterIncrement}>
+          +
+        </button>
+      </div>
     </>
   );
 };
@@ -65,6 +119,13 @@ export const StaffControlPanel = () => {
               <div className={S.controlLabel}>Turn Ariel Off</div>
               <div className={S.controlAction}>
                 <Switchable />
+              </div>
+            </div>
+
+            <div className={S.controlItem}>
+              <div className={S.controlLabel}>Raise Temp</div>
+              <div className={S.controlAction}>
+                <Counter />
               </div>
             </div>
 
